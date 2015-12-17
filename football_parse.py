5dbbeca0bb630ac72fc7a56dbe49351a9904f67b
uallah <afiz2u@gmail.com>
2015-12-17 20:28:27 +0530

    Plot function added for Goals made vs given

--- football_parse.py
+++ football_parse.py
@@ -1,5 +1,17 @@
 #import required packages
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#this functions takes dataset and draws bar chart.
+def plotBar(dataSet,plotType,title,xaxis, yaxis):
+    plt.rcParams['figure.figsize'] = (15, 5)
+    dataSet.plot(kind=plotType)
+    plt.title(title)
+    plt.xlabel(xaxis)
+    plt.ylabel(yaxis)
+    plt.show()
+
 
 #Reading data
 data = pd.read_csv("football.csv")
@@ -8,6 +20,9 @@ print(data.columns)
 
 #select only team, Goals, Goals Allowed
 data_goals = data[['Team','Goals', 'Goals Allowed']]
+
+dd = data[['Team','Goals', 'Goals Allowed']]
+plotBar(dd,"bar", "Goals Made vs Goals Given", "Teams", "Goals")
 
 #Difference between for and against Goals
 data_goals['Diff'] = data_goals['Goals']- data_goals['Goals Allowed']
@@ -20,5 +35,4 @@ data_goals = data_goals.sort_index(by='Abs', ascending= True)
 
 #Top 5 teams with least difference for and against goals
 print(data_goals.head(5))
-
 
